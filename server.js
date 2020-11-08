a67381069c55061da0e794a6ffe2325b735aa90f
pikou <434708082@qq.com>
2020-11-08 21:34:33 +0800

    gzip优化

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const Cp = require('child_process')
 const Koa = require('koa')
-const Static = require('koa-static')
+const Static = require('koa-static-cache')
 
 const app = new Koa()
 app.use(
   Static('./dist', {
+    buffer: false,
     gzip: true,
+    dynamic: true,
   })
 )
 
@@ -15,4 +17,4 @@ app.listen(port)
 
 const url = `http://localhost:${port}`
 console.log(`Server started at ${url}`)
-Cp.exec(`start chrome ${url}`)
+Cp.exec(`start chrome ${url}/index.html`)
